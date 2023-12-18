4f6e07100866cbab4a92261c54f302e14fff2caa
Khomyakov Ivan <Khomyakov.Ivan@russianpost.ru>
2023-12-18 19:20:10 +0300

    Done

--- Spacebattle/Spacebattle/Vector.cs
+++ Spacebattle/Spacebattle/Vector.cs
@@ -1,8 +1,4 @@
-﻿using System.Linq;
-using System;
-using System.Linq.Expressions;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 
 
 namespace Spacebattle
@@ -13,52 +9,56 @@ namespace Spacebattle
 
         public Vector(IEnumerable<T> initialValues)
         {
-            // Задача 1.
+            if (initialValues == null || !initialValues.Any())
+                throw new ArgumentException("Initial values cannot be null or empty.");
+
+            _values = initialValues.ToArray();
         }
 
-        public int Size
-        {
-            get
-            {
-                //Задача 2
-                return 0; // Это заглушка, чтобы компилировался код
-            }
-        }
+        public int Size => _values.Length;
 
         public static implicit operator Vector<T>(T[] a)
         {
-            //Задача 3
-            return null; // Это заглушка, чтобы компилировался код
+            if (a == null || a.Length == 0)
+                throw new ArgumentException("Array cannot be null or empty.");
+
+            return new Vector<T>(a);
         }
 
         IEnumerator<T> IEnumerable<T>.GetEnumerator()
         {
-            //Задача 4
-            return null; // Это заглушка, чтобы компилировался код
+            return ((IEnumerable<T>)_values).GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            //Задача 4
-            return null; // Это заглушка, чтобы компилировался код
+            return _values.GetEnumerator();
         }
 
         public override string ToString()
         {
-            //Задача 5
-            return string.Empty; // Это заглушка, чтобы компилировался код
+            return $"({string.Join(", ", _values)})";
         }
 
         public static T[] Parse(string value, Func<string, T> parse)
         {
-            //Задача 6
-            return new T[0]; // Это заглушка, чтобы компилировался код
+            return value.Trim('(', ')').Split(',')
+                .Select(parse).ToArray();
         }
 
         public static Vector<T> operator +(Vector<T> a, Vector<T> b)
         {
-            //Задача 7
-            return null; // Это заглушка, чтобы компилировался код
+            if (a.Size != b.Size)
+                throw new ArgumentException("Vector dimensions must be equal for addition.");
+
+            T[] result = new T[a.Size];
+
+            for (int i = 0; i < a.Size; i++)
+            {
+                result[i] = (T)Convert.ChangeType((dynamic)a._values[i]! + b._values[i], typeof(T));
+            }
+
+            return new Vector<T>(result);
         }
 
     }
