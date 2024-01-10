8701ed39885c666e6731a26010643c1e84ae90f2
boris.zharkov <boris.zharkov@russianpost.ru>
2024-01-10 19:05:30 +0300

    #HW#1-Zharkov-Boris Vector

--- Spacebattle/Spacebattle/Vector.cs
+++ Spacebattle/Spacebattle/Vector.cs
@@ -14,6 +14,10 @@ namespace Spacebattle
         public Vector(IEnumerable<T> initialValues)
         {
             // Задача 1.
+            if (initialValues == null || initialValues.Count() <= 0)
+                throw new ArgumentException("Пустой массив");
+            
+            _values = initialValues.ToArray();
         }
 
         public int Size
@@ -21,44 +25,72 @@ namespace Spacebattle
             get
             {
                 //Задача 2
-                return 0; // Это заглушка, чтобы компилировался код
+                return _values.Length;
             }
         }
 
         public static implicit operator Vector<T>(T[] a)
         {
             //Задача 3
-            return null; // Это заглушка, чтобы компилировался код
+            return new Vector<T>(a);
         }
 
-        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        public IEnumerator<T> GetEnumerator()
         {
             //Задача 4
-            return null; // Это заглушка, чтобы компилировался код
+            foreach (var value in _values)
+            {
+                yield return value;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             //Задача 4
-            return null; // Это заглушка, чтобы компилировался код
+            return GetEnumerator();
         }
 
         public override string ToString()
         {
             //Задача 5
-            return string.Empty; // Это заглушка, чтобы компилировался код
+            return $"({string.Join(", ", _values)})";
         }
 
         public static T[] Parse(string value, Func<string, T> parse)
         {
             //Задача 6
-            return new T[0]; // Это заглушка, чтобы компилировался код
+            var trimValues = value.Trim('(', ')').Split(',');
+
+            var data = new T[trimValues.Length];
+            for (int i = 0; i < trimValues.Length; i++)
+            {
+                data[i] = parse(trimValues[i].Trim());
+            }
+
+            return data;
         }
 
         public static Vector<T> operator +(Vector<T> a, Vector<T> b)
         {
             //Задача 7
-            return null; // Это заглушка, чтобы компилировался код
+            if(a.Size != b.Size)
+                throw new ArgumentException("Размерности векторов не совпадают");
+            
+            ParameterExpression ap = Expression.Parameter(typeof(T), "a");
+            ParameterExpression bp = Expression.Parameter(typeof(T), "b");
+            Expression operationResult = Expression.Add(ap, bp);
+            
+            var lambda = Expression.Lambda<Func<T, T, T>>(operationResult, ap, bp);
+
+            Func<T, T, T> addFunc = lambda.Compile();
+            
+            var data = new T[a.Size];
+            for (int i = 0; i < a.Size; i++)
+            {
+                data[i] = addFunc(a._values[i], b._values[i]);
+            }
+
+            return data;
         }
 
     }
