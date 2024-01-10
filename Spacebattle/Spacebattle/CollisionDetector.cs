bc67458e4b69a21b2b7dff5f055ede766f59c1cb
boris.zharkov <boris.zharkov@russianpost.ru>
2024-01-10 19:11:03 +0300

    HW#2-Zharkov-Boris Delegate

--- Spacebattle/Spacebattle/CollisionDetector.cs
+++ Spacebattle/Spacebattle/CollisionDetector.cs
@@ -8,14 +8,51 @@ namespace Spacebattle
 {
     public class CollisionDetector<T>
     {
+        public delegate void DetectedHandler<T>(T val);
+
+        public DetectedHandler<T> Detected;
+
+        private Dictionary<T, CollisionDetector<T>> node;
+
+        public CollisionDetector()
+        {
+            node = new Dictionary<T, CollisionDetector<T>>();
+        }
+        
         public void Add(IEnumerable<T> sample)
         {
-
+            var currentNode = this;
+            foreach (var val in sample)
+            {
+                if (!currentNode.node.ContainsKey(val))
+                {
+                    currentNode.node[val] = new CollisionDetector<T>();
+                }
+                currentNode = currentNode.node[val];
+            }
         }
 
         public void Detect(IEnumerable<T> pattern)
         {
+            var currentNode = this;
+            foreach (var val in pattern)
+            {
+                if (currentNode.node.ContainsKey(val))
+                {
+                    currentNode = currentNode.node[val];
 
+                    if (!currentNode.node.Any())
+                    {
+                        // Коллизия - вызываем событие Detected
+                        Detected?.Invoke(val);
+                        return;
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
         }
     }
 }
