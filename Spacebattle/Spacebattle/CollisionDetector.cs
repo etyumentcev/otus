9edbe2793e29f5882955e3e39429189d76321527
Владислав Иоффе <Vladislav.Ioffe@russianpost.ru>
2024-01-15 01:00:48 +0300

    Add realization with no delegate

--- Spacebattle/Spacebattle/CollisionDetector.cs
+++ Spacebattle/Spacebattle/CollisionDetector.cs
@@ -1,20 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Spacebattle
+﻿namespace Spacebattle
 {
     public class CollisionDetector<T>
     {
+        public event Action OnCollisionDetected;
+
+        private PrefixTreeNode<T> _treeRootNode;
+
+        public CollisionDetector() 
+        {
+            _treeRootNode = new PrefixTreeNode<T>(default(T), false);
+        }
+
         public void Add(IEnumerable<T> sample)
         {
+            var currentNode = _treeRootNode;
+            for (var i = 0; i < sample.Count(); i++)
+            {
+                if (i == sample.Count() - 1)
+                {
+                    currentNode.AddChildren(sample.ElementAt(i), true);
+                    break;
+                }
 
+                if (currentNode.Children.Any(_ => _.Key.Equals(sample.ElementAt(i))))
+                {
+                    var child = currentNode.Children.Where(_ => _.Key.Equals(sample.ElementAt(i))).FirstOrDefault();
+                    currentNode = child.Value;
+                    continue;
+                }
+
+                currentNode = currentNode.AddChildren(sample.ElementAt(i), false);
+            }
         }
 
         public void Detect(IEnumerable<T> pattern)
         {
+            var currentNode = _treeRootNode;
+            for (var i = 0; i <= pattern.Count(); i++)
+            {
+                if (i == pattern.Count())
+                {
+                    if (currentNode.IsEndNode)
+                    {
+                        OnCollisionDetected?.Invoke();
+                    }
+
+                    break;
+                }
+
+                if (currentNode.Children.Any(_ => _.Key.Equals(pattern.ElementAt(i))))
+                {
+                    var child = currentNode.Children.Where(_ => _.Key.Equals(pattern.ElementAt(i))).FirstOrDefault();
+                    currentNode = child.Value;
+                    continue;
+                }
+
+                break;
+            }
 
         }
     }
---
+++ Spacebattle/Spacebattle/PrefixTreeNode.cs
@@ -0,0 +1,25 @@
+﻿namespace Spacebattle
+{
+    internal class PrefixTreeNode<T>
+    {
+        public PrefixTreeNode(T value, bool isEndNode) 
+        {
+            Value = value;
+            IsEndNode = isEndNode;
+
+            Children = new Dictionary<T, PrefixTreeNode<T>>();
+        } 
+        public T Value { get; }
+
+        public bool IsEndNode { get; }
+
+        public Dictionary<T, PrefixTreeNode<T>> Children { get; }
+
+        public PrefixTreeNode<T> AddChildren(T value, bool isEndNode)
+        {
+            Children.Add(value, new PrefixTreeNode<T>(value, isEndNode));
+
+            return Children[value];
+        }
+    }
+}
