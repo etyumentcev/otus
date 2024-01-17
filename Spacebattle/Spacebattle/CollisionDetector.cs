3d28816b6da04ee786897ad655f16c07179064c6
DESKTOP-I94MI2F\user <iiipoizone@gmail.com>
2024-01-17 17:01:03 +0400

    new DZ-2

--- Spacebattle/Spacebattle/CollisionDetector.cs
+++ Spacebattle/Spacebattle/CollisionDetector.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,14 +9,91 @@ namespace Spacebattle
 {
     public class CollisionDetector<T>
     {
+        Trie<int> trie;
+
+        private List<IEnumerable<T>> samples;
+        public event Action? CollisionDetected;
+
+        public CollisionDetector()
+        {
+            trie = new Trie<int>();
+            samples = new List<IEnumerable<T>>();
+        }
+
         public void Add(IEnumerable<T> sample)
         {
-
+            IEnumerable<int> i = (IEnumerable<int>)sample;
+            trie.Add(i);
         }
 
         public void Detect(IEnumerable<T> pattern)
         {
+            if (trie.Detect((IEnumerable<int>)pattern))
+            {
+                OnCollisionDetected();
+            }
+        }
 
+        void OnCollisionDetected()
+        {
+            CollisionDetected?.Invoke();
+        }
+
+    }
+    public class TrieNode<T>
+    {
+        public Dictionary<T, TrieNode<T>> Children { get; set; }
+        public bool IsEndOfWord { get; set; }
+
+        public TrieNode()
+        {
+            Children = new Dictionary<T, TrieNode<T>>();
+            IsEndOfWord = false;
         }
     }
+
+    public class Trie<T>
+    {
+        private TrieNode<T> root;
+
+        public Trie()
+        {
+            root = new TrieNode<T>();
+        }
+
+        public void Add(IEnumerable<T> values)
+        {
+            TrieNode<T> node = root;
+            foreach (T value in values)
+            {
+                if (!node.Children.ContainsKey(value))
+                {
+                    node.Children[value] = new TrieNode<T>();
+                }
+                node = node.Children[value];
+            }
+            node.IsEndOfWord = true;
+        }
+
+        public bool Detect(IEnumerable<T> value)
+        {
+            TrieNode<T> node = root;
+            foreach (T val in value)
+            {
+                if (val is int)
+                {
+
+                    if (!node.Children.ContainsKey(val))
+                    {
+                        return false;
+                    }
+                    node = node.Children[val];
+                }
+            }
+            return node.IsEndOfWord;
+        }
+    }
+
+
+
 }
