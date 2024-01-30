6f1ca86a1248e92246204cf4eb2e360fc791166e
Sabitov Renat <Renat.Sabitov@russianpost.ru>
2024-01-30 09:37:27 +0300

    HW#2-SabitovRenat

--- Spacebattle/Spacebattle/CollisionDetector.cs
+++ Spacebattle/Spacebattle/CollisionDetector.cs
@@ -1,21 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Spacebattle
+﻿namespace Spacebattle
 {
     public class CollisionDetector<T>
     {
+        private Dictionary<T, CollisionDetector<T>> nodes;
+        public event Action Detected;
+
+        public CollisionDetector()
+        {
+            nodes = new Dictionary<T, CollisionDetector<T>>();
+        }
+
         public void Add(IEnumerable<T> sample)
         {
+            CollisionDetector<T> current = this;
 
+            foreach (var item in sample)
+            {
+                if (!current.nodes.ContainsKey(item))
+                {
+                    current.nodes[item] = new CollisionDetector<T>();
+                }
+                current = current.nodes[item];
+            }
+            current.Detected += () => Detected?.Invoke();
         }
 
         public void Detect(IEnumerable<T> pattern)
         {
+            CollisionDetector<T> current = this;
 
+            foreach (var item in pattern)
+            {
+                if (current.nodes.ContainsKey(item))
+                {
+                    current = current.nodes[item];
+                }
+                else
+                {
+                    return;
+                }
+            }
+            current.Detected?.Invoke();
         }
     }
 }
