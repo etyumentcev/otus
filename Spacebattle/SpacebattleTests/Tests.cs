c5b5c14aa75421b71d3388306ec82f176f3be0ed
YagovkinSI <sorpionupgrade@mail.ru>
2024-03-06 07:35:04 +0400

    [HW#5] Add interface ICommand and simple test

---
+++ Spacebattle/SpacebattleTests/Tests.cs
@@ -0,0 +1,36 @@
+﻿using Spacebattle.Interfaces;
+
+public class CommandExecutorTests
+{
+    [Fact]
+    public void ICommand_ShouldStarCommand()
+    {
+        // Arrange
+        var command = new TestCommand(1);
+
+        // Act
+        command.Execute();
+
+        // Assert
+        Assert.True(command.StateComplete);
+    }
+
+    private class TestCommand : ICommand
+    {
+        private readonly int _id;
+        public TestCommand(int id)
+        {
+            _id = id;
+        }
+
+        public bool StateComplete { get; private set; }
+
+        public void Execute()
+        {
+            Console.WriteLine($"Запуск выполнения команды №{_id}");
+            Thread.Sleep(500);
+            StateComplete = true;
+            Console.WriteLine($"Команда №{_id} выполнена");
+        }
+    }
+}
