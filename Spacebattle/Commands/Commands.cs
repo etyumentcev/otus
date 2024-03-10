fe3f3278b66636345316a574716bd2b55ad1503b
Andrey.Ishchenko <Andrey.Ishchenko@russianpost.ru>
2024-03-10 20:04:26 +0300

    HW#5-многопоточное выполнение команд

---
+++ Spacebattle/Commands/Commands.cs
@@ -0,0 +1,56 @@
+﻿namespace Commands;
+
+/// <summary>
+/// Создание и запуск Workerа в рамках которого будет запущен поток с циклом выполнения задач
+/// </summary>
+public class RunWorkerCommand: ICommand
+{
+    public Worker Worker { get; }
+
+    public RunWorkerCommand()
+    {
+        Worker = new Worker();
+    }
+
+    /// <summary>
+    /// Старт цикла выполнения команд
+    /// </summary>
+    public void Execute()
+    {
+        Worker.WThread.Start();
+    }
+}
+
+/// <summary>
+/// Остановка цикла выполнения команд, дожидаясь их полного завершения 
+/// </summary>
+public class SoftStopCommand: ICommand
+{
+    private readonly CancellationTokenSource _cts;
+
+    public SoftStopCommand(Worker worker)
+    {
+        _cts = worker.CtsSoft;
+    }
+    public void Execute()
+    {
+        _cts.Cancel();
+    }
+}
+
+/// <summary>
+/// Остановка цикла выполнения команд, не дожидаясь их полного завершения 
+/// </summary>
+public class HardStopCommand: ICommand
+{
+    private readonly CancellationTokenSource _cts;
+
+    public HardStopCommand(Worker worker)
+    {
+        _cts = worker.CtsHard;
+    }
+    public void Execute()
+    {
+        _cts.Cancel();
+    }
+}
---
+++ Spacebattle/Commands/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+
+namespace Commands;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var runCommand = new RunWorkerCommand();
+        runCommand.Execute();
+        
+        //  Вывод двух строк "Команда вывода строки".
+        // RunCommand.Worker.AddCommandToQueue(new WriteLineCommand());
+        // RunCommand.Worker.AddCommandToQueue(new SoftStopCommand(RunCommand.Worker));
+        // RunCommand.Worker.AddCommandToQueue(new WriteLineCommand());
+        
+        
+        //  Вывод одной строки "Команда вывода строки".
+        runCommand.Worker.AddCommandToQueue(new WriteLineCommand());
+        runCommand.Worker.AddCommandToQueue(new HardStopCommand(runCommand.Worker));
+        runCommand.Worker.AddCommandToQueue(new WriteLineCommand());
+    }
+}
